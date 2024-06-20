60a603db863dfe0e0fb0dd3d17fbe4dfa13b6865
Shayna Provine <shayvana@Shaynas-MacBook-Air.local>
2024-06-19 21:18:12 -1000

    Update Flask app to use the correct port and configure gunicorn

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template_string, request
 import sqlite3
+import os
 
 app = Flask(__name__)
 
@@ -50,4 +51,5 @@ def generate_blog_template(user, posts, query=None):
     '''
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
